e4e37c37958130870856ee9001214488d3ad7bce
Aloft661 <3036404130z@gmail.com>
2026-03-21 21:20:11 +0800

    响应拦截器、mockjs、跨域代理

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
+import "./mock";
 import Vue from 'vue';
 import App from './App.vue';
 import "./styles/global.less";
 import router from './router';
 import showMessage from './utils/showMessage';
-
 Vue.prototype.$showMessage = showMessage;
 new Vue({
   router,
