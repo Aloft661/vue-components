94e2fd5c5dd7bf28394edeb8bcc5646ce8d6e244
Aloft661 <3036404130z@gmail.com>
2026-03-21 17:35:52 +0800

    消息弹出框并挂载到Vue实例；在Vue中使用ref获取组件实例

---
+++ src/utils/showMessage.js
@@ -0,0 +1,45 @@
+import getComponentRootDom from "./getComponentRootDom";
+import Icon from '@/components/Icon';
+import styles from './showMessage.module.less';
+/**
+ * 弹出消息
+ * @param {String} content 消息内容
+ * @param {String} type 消息类型 info error success warn
+ * @param {Number} duration 多久后消失
+ * @param {HTMLElement} container 容器，消息会显示到该容器的正中间；如果不传，则显示到页面正中间
+ */
+export default function (options = {}) {
+    const {
+        content = '',
+        type = 'info',
+        duration = 2000,
+        container = document.body
+    } = options;
+    // 创建消息元素
+    const div = document.createElement('div');
+    const iconDom = getComponentRootDom(Icon, { 
+        type 
+    });
+
+    div.innerHTML = `<span class="${styles.icon}">${iconDom.outerHTML}</span><div>${content}</div>`;
+    div.className = `${styles.message} ${styles["message-" + type]}`;
+    
+    if (getComputedStyle(container).position === 'static') {
+        container.style.position = 'relative';
+    }
+
+    container.appendChild(div);
+    div.clientHeight;
+
+    div.style.opacity = 1;
+    div.style.transform = `translate(-50%, -50%)`;
+
+    setTimeout(() => {
+        div.style.opacity = 0;
+        div.style.transform = `translate(-50%, -50%) translateY(-15px)`;
+        div.addEventListener("transitionend", function () {
+            div.remove();
+            options.callback && options.callback();
+        }, { once: true });
+    }, duration);
+}
